c6e3e5c2eeee189971efd20b0780f5740d1b1161
Oyin414 <kadirioyin88@gmail.com>
2026-05-21 13:15:24 +0100

    feat:add hash method to Hashmap class

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,6 +1,17 @@
-class hashmap {
+export class Hashmap {
   constructor() {
     let loadFactor = 0.75;
     let capacity = 16;
+  }
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % 16;
+    }
+
+    return hashCode;
   }
 }
